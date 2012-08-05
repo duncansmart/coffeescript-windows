93b6e7abd39d7b0c38147b4c0b51ebc1fd4152ef
Duncan Smart <duncan.smart@gmail.com>
2012-08-05 20:20:06 +0100

    UTF-8 reading/writing code from @adamschoenemann

--- test/test.cmd
+++ test/test.cmd
@@ -2,11 +2,13 @@
 setlocal ENABLEDELAYEDEXPANSION
 
 :: We assume that a successful compile of each file is sufficient
+set TEMP_RESULT_JS=%TEMP%\coffeescript-windows-%RANDOM%.js
 
 for %%F in (*.coffee) do (
     echo %%F
-    call ..\coffee "%%F" > nul
+    call ..\coffee "%%F" "%TEMP_RESULT_JS%"
     if !ERRORLEVEL! neq 0 echo *** %%F FAILED *** & exit /b
 )
 echo *** PASSED ***
 
+del "%TEMP_RESULT_JS%"
